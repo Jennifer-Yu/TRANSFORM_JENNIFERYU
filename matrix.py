7e7645b344ba359013d6840272b72b85152e48a7
Jennifer <jyu6@stuy.edu>
2017-03-08 23:00:28 -0500

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,19 +1,70 @@
 import math
 
+# 1 0 0 x
+# 0 1 0 y
+# 0 0 1 z
+# 0 0 0 1
 def make_translate( x, y, z ):
-    pass
+    temp = new_matrix()
+    ident(temp)
+    temp[3][0] = x #account for 0 col / row
+    temp[3][1] = y
+    temp[3][2] = z
+    return temp
 
+# x 0 0 0
+# 0 y 0 0
+# 0 0 z 0
+# 0 0 0 1
 def make_scale( x, y, z ):
-    pass
+    temp = new_matrix()
+    ident(temp)
+    temp[0][0] = x
+    temp[1][1] = y
+    temp[2][2] = z
+    return temp
 
-def make_rotX( theta ):    
-    pass
+# 1 0 0 0
+# 0 cos -sin 0
+# 0 sin cos 0
+# 0 0 0 1
+def make_rotX( theta ):
+    temp = new_matrix()
+    ident(temp)
+    theta = math.radians(theta)
+    temp[1][1] = math.cos(theta)
+    temp[1][2] = math.sin(theta)
+    temp[2][1] = -math.sin(theta)
+    temp[2][2] = math.cos(theta)
+    return temp
 
+# cos 0 sin 0
+# 0 1 0 0
+# -sin 0 cos 0
+# 0 0 0 1
 def make_rotY( theta ):
-    pass
+    temp = new_matrix()
+    ident(temp)
+    theta = math.radians(theta)
+    temp[0][0] = math.cos(theta)
+    temp[0][2] = -math.sin(theta)
+    temp[2][0] = math.sin(theta)
+    temp[2][2] = math.cos(theta)
+    return temp
 
+# cos -sin 0 0
+# sin cos 0 0
+# 0 0 1 0
+# 0 0 0 1
 def make_rotZ( theta ):
-    pass
+    temp = new_matrix()
+    ident(temp)
+    theta = math.radians(theta)
+    temp[0][0] = math.cos(theta)
+    temp[0][1] = math.sin(theta)
+    temp[1][0] = -math.sin(theta)
+    temp[1][1] = math.cos(theta)
+    return temp
 
 def print_matrix( matrix ):
     s = ''
@@ -35,7 +86,7 @@ def scalar_mult( matrix, s ):
     for r in range( len( matrix[0] ) ):
         for c in range( len(matrix) ):
             matrix[c][r]*= s
-            
+
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
 
@@ -43,7 +94,7 @@ def matrix_mult( m1, m2 ):
     for row in m2:
         #get a copy of the next point
         tmp = row[:]
-        
+
         for r in range(4):
             m2[point][r] = (m1[0][r] * tmp[0] +
                             m1[1][r] * tmp[1] +
